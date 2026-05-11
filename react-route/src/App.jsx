37b0ce1da88268c3b21bc5109fd5af153cdeb5b8
Ana <ana.j.ferreira15@aluno.senai.br>
2026-05-11 14:11:40 -0300

    Feat: Finaliza a atividade

--- react-route/src/App.jsx
+++ react-route/src/App.jsx
@@ -1,5 +1,12 @@
-import { BrowserRouter, Router,  Route } from 'react-router-dom';
+import { BrowserRouter, Routes,  Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
+import Home from './pages/Home/Home';
+import Sobre from './pages/Sobre/Sobre';
+import Servicos from './pages/Servicos/Servicos';
+import Contato from './pages/Contato/Contato';
+import Extra from './pages/Extra/Extra';
+import NotFound from './pages/NotFound/NotFound';
 
 function App() {
   return (
@@ -11,7 +18,7 @@ function App() {
       <Route path="/sobre" element={<Sobre/>} />
       <Route path="/servicos" element={<Servicos/>} />
       <Route path="/contato" element={<Contato/>} />
-      <Route path="/extra" element={<NotFound/>} />
+      <Route path="/extra" element={<Extra/>} />
       <Route path="*" element={<NotFound/>} />
     </Routes>
     </main>
